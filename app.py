2baaa95ffea991e68f752617f4a6171d10b9d465
Falko <16203265+universalamateur@users.noreply.github.com>
2021-11-21 00:00:35 +0200

    Added route to receive status updates from Twilio

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from twilio.twiml.messaging_response import MessagingResponse
 app = Flask(__name__)
 
 # Create a route to handle incoming SMS messages
-# This is where the magic happens!
+# It replies with a Sms message with the string returned
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     print(f'Incoming message from {request.values.get("From")}: ${request.values.get("Body")}')
 
-    # Here, we're generating TwiML using the Python helper library
+    # Generating TwiML using the Python helper library
     resp = MessagingResponse()
     resp.message("TwilioQuest rules")
 
     return str(resp)
+
+# Receiving the status updates on the sent SMS
+@app.route("/status", methods=["GET", "POST"])
+def my_status_function():
+    print(f"Message SID {request.values.get('MessageSid')} has a status of {request.values.get('MessageStatus')}")
